393e3dac89afce32ead7a3f7c668658ec21b4055
géballin <macniaque@free.fr>
2019-02-15 17:44:49 +0100

    Replace generateStarpack shell script by a batch file, more windows friendly.

---
+++ generateStarpack.bat
@@ -0,0 +1,12 @@
+REM BATCH script to generate windows Starpack of PgBrowse
+REM sdx.kit must be in ../ as well as your tclkit
+
+mkdir PgBrowse.vfs
+copy pgBrowse.tcl PgBrowse.vfs\main.tcl
+copy README.md PgBrowse.vfs\
+mkdir PgBrowse.vfs\Support
+xcopy Support PgBrowse.vfs\Support\ /s
+move PgBrowse.vfs\Support\icone.ico PgBrowse.vfs\tclkit.ico
+move PgBrowse.vfs\Support\tclkit.inf PgBrowse.vfs\tclkit.inf
+
+tclsh ../sdx.kit wrap PgBrowse.exe -runtime ../kbsvq8.6-bi-0.4.7.exe
